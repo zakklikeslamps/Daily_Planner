b3bf023ce170383391c6d03e8dcc8f42bfa469c1
Zakk Haug <zakklikeslamps@gmail.com>
2021-01-27 23:36:05 -0500

    Removed a file causing an issue. Also updated script.js page; now contains an array w the hours.

--- Develop/Assets/js/script.js
+++ Develop/Assets/js/script.js
@@ -1,77 +1,98 @@
-//Keeps the script from loading until the entire page has loaded
+//Keeps script from loading until the page has fully loaded. 
 $(document).ready(function () {
-
-    //Setting local date & appending to HTML header
+    //Current Date via moment.JS
     const currentDay = moment().format("MMMM Do YYYY");
     console.log(currentDay);
     $("#currentDay").append(currentDay);
-
     const present = parseInt(moment().format("HH"));
     console.log(present)
 
-    var $text8AM = $("#text8AM");
-    var $text9AM = $("#text9AM");
-    var $text10AM = $("#text10AM");
-    var $text11AM = $("#text11AM");
-    var $text12AM = $("#text12AM");
-    var $text1PM = $("#text1PM");
-    var $text2PM = $("#text2PM");
-    var $text3PM = $("#text3PM");
-    var $text4PM = $("#text4PM");
-    var $text5PM = $("#text5PM");
-    var $text6PM = $("#text6PM");
-    var $text7PM = $("#text7PM");
+    var currentHour = moment().format('LT');
+
+    //Array of hour slots
+    var slots = [8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7];
+
+
+
+
+
+
+
+    //// if it is a past hour we want a grey background 
+
+    /*for (i = 9; i < 18; i++) {
+        var timeCheck = "#area" + i
+        console.log(timeCheck)
+    
+        $(timeCheck).css("background-color", "grey")
+    }
+    //// if it is a future  hour we want a green background 
+    for (i = 9; i < 18; i++) {
+        var timeCheck = "#area" + i
+        console.log(timeCheck)
+        if (i > presentHour) {
+            $(timeCheck).css("background-color", "green")
+        }
+    }*/
+
 
     $("textarea").each(function () {
-        var name = parseInt($(this).attr("name"));
-        if (name < present) {
+        var time = parseInt($(this).attr("name"));
+        if (time < present) {
             $(this).addClass("past");
         }
 
-
-        if (name > present) {
-            $(this).addClass("future")
-        }
-
-        if (name === present) {
+        else if (time === present) {
             $(this).addClass("present")
         }
 
+        else {
+            $(this).addClass("future")
+        }
+
 
 
     })
 
 
+    //save button
     $("button").on("click", function () {
 
-        //sets items to the local storage
-        localStorage.setItem("8:00", ($text8AM.val()))
-        localStorage.setItem("9:00", ($text9AM.val()))
-        localStorage.setItem("10:00", ($text10AM.val()))
-        localStorage.setItem("11:00", ($text11AM.val()))
-        localStorage.setItem("12:00", ($text12AM.val()))
-        localStorage.setItem("13:00", ($text1PM.val()))
-        localStorage.setItem("14:00", ($text2PM.val()))
-        localStorage.setItem("15:00", ($text3PM.val()))
-        localStorage.setItem("16:00", ($text4PM.val()))
-        localStorage.setItem("17:00", ($text5PM.val()))
-        localStorage.setItem("18:00", ($text6PM.val()))
-        localStorage.setItem("19:00", ($text7PM.val()))
+        var hour = $(this).parent().attr("id")
+        var value = $(this).siblings("textArea").val().trim()
+        localStorage = setItem(hour, value);
+
+        //Redundant block//
+        /*localStorage.setItem("8:00", text8AM.val())
+        localStorage.setItem("9:00", text9AM.val())
+        localStorage.setItem("10:00", text10AM.val())
+        localStorage.setItem("11:00", text11AM.val())
+        localStorage.setItem("12:00", text12AM.val())
+        localStorage.setItem("13:00", text1PM.val())
+        localStorage.setItem("14:00", text2PM.val())
+        localStorage.setItem("15:00", text3PM.val())
+        localStorage.setItem("16:00", text4PM.val())
+        localStorage.setItem("17:00", text5PM.val())
+        localStorage.setItem("18:00", text6PM.val())
+        localStorage.setItem("19:00", text7PM.val())*/
     })
 
-    //Storing content & displaying it on screen; content should (but isn't) remain when page is refreshed
-    $("#text8AM").append(localStorage.getItem("8:00"));
-    $("#text9AM").append(localStorage.getItem("9:00"));
-    $("#text10AM").append(localStorage.getItem("10:00"));
-    $("#text11AM").append(localStorage.getItem("11:00"));
-    $("#text12AM").append(localStorage.getItem("12:00"));
-    $("#text1PM").append(localStorage.getItem("13:00"));
-    $("#text2PM").append(localStorage.getItem("14:00"));
-    $("#text3PM").append(localStorage.getItem("15:00"));
-    $("#text4PM").append(localStorage.getItem("16:00"));
-    $("#text5PM").append(localStorage.getItem("17:00"));
-    $("#text6PM").append(localStorage.getItem("18:00"));
-    $("#text7PM").append(localStorage.getItem("19:00"));
-
+    //Displaying content
+    slots.forEach(function (hour) {
+        $('#${hour} .description').val(localStorage.getItem(hour));
+    })
+    //Redundant block//
+    $("#8").val(localStorage.getItem("8:00"));
+    $("#9").val(localStorage.getItem("9:00"));
+    $("#10").val(localStorage.getItem("10:00"));
+    $("#11").val(localStorage.getItem("11:00"));
+    $("#12").val(localStorage.getItem("12:00"));
+    $("#1").val(localStorage.getItem("13:00"));
+    $("#2").val(localStorage.getItem("14:00"));
+    $("#3").val(localStorage.getItem("15:00"));
+    $("#4").val(localStorage.getItem("16:00"));
+    $("#5").val(localStorage.getItem("17:00"));
+    $("#6").val(localStorage.getItem("18:00"));
+    $("#7").val(localStorage.getItem("19:00"));
 
 })
